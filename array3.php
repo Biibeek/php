f49758e2b490170c25c73d9fda88df4a84ac31ce
Bibek Bhurtel <bhurtellbbek@gmail.com>
2023-10-18 20:09:04 +0545

    array

---
+++ array3.php
@@ -0,0 +1,19 @@
+<?php
+//1.Numeric Array
+//creating an array
+$fruit_name=array("apple","banana");
+//printing an array
+print_r($fruit_name); 
+echo '<br/>';
+
+//2.Associative Array
+$associative_array=array(
+    "Zack"=>"Zara",
+    "Anthony"=>"Any",
+    "Ram"=>"Rani",
+    "salim"=>"Sara",
+    "Raghav"=>"Ravina"
+);
+print_r($associative_array);
+echo '<br/>'
+?>
