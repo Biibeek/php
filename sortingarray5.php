f6cf834bf4f1670a37ff3ef2ab5e07f57b3d111f
Bibek Bhurtel <bhurtellbbek@gmail.com>
2023-10-19 19:26:59 +0545

    types of sort in php

--- sortingarray5.php
+++ sortingarray5.php
@@ -1,12 +1,28 @@
 <?php
+//using asort.i.e sorting array by element value in descending order
 $meal= array('breakfast '=> 'walnut bun',
-'launch => cashew nuts and white mushroom',
+'launch' => 'cashew nuts and white mushroom',
 'snack' => 'dried mulberries',
-'diiner' => 'rice');
+'dinner' => 'rice');
 echo "before sorting</br>";
 print_r($meal);
 echo '<br/>';
 asort($meal);
+echo "after sorting<br>";
+print_r($meal);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+//using ksort i.e sorting an array by keys in ascending order
+$meal= array('breakfast '=> 'walnut bun',
+'launch' => 'cashew nuts and white mushroom',
+'snack' => 'dried mulberries',
+'dinner' => 'rice');
+echo "before sorting</br>";
+print_r($meal);
+echo '<br/>';
+ksort($meal);
 echo "after sorting<br>";
 print_r($meal);
 
